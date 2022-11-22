dfa89bce6e39a622a0c052778a66c02faeff7246
Catherine Joy Calupas <cat.calupas@gmail.com>
2022-11-22 03:38:38 +0100

    add shader graph

---
+++ 3D Test Project/Assets/Scripts/SampleCamera.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleCamera : MonoBehaviour
+{
+    // Movement of this camera using W, A, S, D
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
